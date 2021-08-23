f8606b24227edb43dd6e06007f6caa91add7a783
Dawsinb <dawsinb@users.noreply.github.com>
2021-08-23 14:25:46 -0400

    Analyser Documentation
    Fill out documentation comments for Analyser

--- src/Analyser.ts
+++ src/Analyser.ts
@@ -11,7 +11,15 @@ export interface AnalyserOptions {
    */
   threshold?: number;
   /**
-   * TODO add desc
+   * Callback function that executes when {@link Analyser.signal} turns from false to true
+   */
+  onSignal?: () => void;
+  /**
+   * Callback function that executes when {@link Analyser.signal} turns from true to false
+   */
+  offSignal?: () => void;
+  /**
+   * Number of frequency bands to split the raw {@link Analyser.frequency} data into
    */
   numberOfBands?: number;
   /**
@@ -46,6 +54,8 @@ export interface AnalyserOptions {
 /** @internal */
 export const AnalyserOptionsDefaults: Required<AnalyserOptions> = {
   threshold: 0.2,
+  onSignal: () => undefined,
+  offSignal: () => undefined,
   numberOfBands: 5,
   fftSize: 2048,
   minDecibels: -100,
@@ -209,9 +219,19 @@ export class Analyser extends EuphonyNode {
   }
   /** @internal  */
   #signal: boolean;
+  /**
+   * Callback function that executes when {@link Analyser.signal} turns from false to true
+   */
+  onSignal: () => void;
+  /**
+   * Callback function that executes when {@link Analyser.signal} turns from true to false
+   */
+  offSignal: () => void;
 
   /**
-   * TODO: add desc
+   * Frequency data by band intervals. Bands are divided up on a log-scale according to {@link Analyser.numberOfBands}.
+   * 
+   * Takes the highest frequency in each band as the intensity for that band
    */
   get bands(): Float32Array {
     return this.#bands;
@@ -219,7 +239,7 @@ export class Analyser extends EuphonyNode {
   /** @internal */
   #bands: Float32Array = new Float32Array();
   /**
-   * TODO: add desc
+   * Number of frequency bands to split the raw {@link Analyser.frequency} data into
    */
   set numberOfBands(count: number) {
     // check if given number is within bounds
@@ -287,6 +307,10 @@ export class Analyser extends EuphonyNode {
     // init threshold to given option param
     this.#threshold = updatedOptions.threshold;
 
+    // init signal callbacks
+    this.onSignal = updatedOptions.onSignal;
+    this.offSignal = updatedOptions.offSignal;
+
     // update number of bands
     this.numberOfBands = updatedOptions.numberOfBands;
 
@@ -297,34 +321,46 @@ export class Analyser extends EuphonyNode {
   /* Functions */
 
   /**
-   * Updates the data for {@link Analyser.frequency}, {@link Analyser.amplitude}, and {@link Analyser.signal}
+   * Updates the data for {@link Analyser.frequency}, {@link Analyser.bands}, {@link Analyser.amplitude}, and {@link Analyser.signal}
    */
   updateFrequency(): void {
     // get raw frequency data (scale is 0-255)
     this._analyserNode.getByteFrequencyData(this._frequencyBuffer);
+    // reset band data
+    this.#bands.fill(0);
 
-    // record normalized frequency data as well as a sum of all raw frequency data
+    // record frequency data
     let sum = 0;
     for (let i = 0; i < this.frequencyBinCount; i++) {
-      this.frequency[i] = this._frequencyBuffer[i] / 255;
+      // add data to amplitude sum
       sum += this._frequencyBuffer[i];
+
+      // record normalized raw frequency data
+      this.#frequency[i] = this._frequencyBuffer[i] / 255;
+
+      // get band index
+      const band = this._bandIntervals.findIndex((interval) => {
+        return interval > i;
+      });
+      // if current value is greater than previous then replace it
+      this.#bands[band] = Math.max(this.#bands[band], this.#frequency[i]);
     }
     // record mean normalized frequency data
     this.#amplitude = sum / this.frequencyBinCount / 255;
 
     // update signal
-    this.#signal = this.amplitude >= this.threshold;
-
-    // record band data
-    this.#bands.fill(0);
-    for (let i = 0; i < this.frequencyBinCount; i++) {
-      // get band index
-      const band = this._bandIntervals.findIndex((interval) => {
-        return interval > i;
-      });
-
-      // if current value is greater than previous then replace it
-      this.#bands[band] = Math.max(this.#bands[band], this.#frequency[i]);
+    if (this.#amplitude >= this.#threshold) {
+      // if signal was previously false then trigger onSignal function
+      if (!this.#signal) {
+        this.onSignal();
+      }
+      this.#signal = true;
+    } else {
+      // if signal was previously true then trigger offSignal function
+      if (this.#signal) {
+        this.offSignal();
+      }
+      this.#signal = false;
     }
   }
 
@@ -337,7 +373,7 @@ export class Analyser extends EuphonyNode {
   }
 
   /**
-   * TODO: add desc
+   * Calculates and records the log-scale intervals for the frequency bands
    * @internal
    */
   private calcBandIntervals(): Array<number> {
